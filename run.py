91701656b5a16c39755610bde3d2c76e15abc67d
mohan Gautam <rajesh.gautam010@gmail.com>
2021-11-03 18:06:42 +0100

    initial commit

--- run.py
+++ run.py
@@ -9,8 +9,6 @@ with Discount_manager() as bot:
 bot.land_first_page(link_to_be_searched)
 bot.implicitly_wait(15)
 bot.agree_fn()
-   # bot.change_currency(currency='USD') 
-   
 rand_var = (float(input("What is the price below which you want to get notified of? \n")))
 bot.price_check(rand_var)
 
